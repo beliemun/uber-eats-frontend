5586cfa171710aaf5a2ff0931a0f0436f3efcab1
beliemun <beliemun@paran.com>
2021-11-20 22:38:26 +0900

    #15.2 React Hook Form part Two

--- src/router/logged-in-router.tsx
+++ src/router/logged-in-router.tsx
@@ -1,3 +1,15 @@
 import React from "react";
+import { isLoggedInVar } from "../apollo";
 
-export const LoggedInRouter = () => <>"Logged In"</>;
+export const LoggedInRouter = () => {
+  const onClink = () => {
+    isLoggedInVar(false);
+  };
+
+  return (
+    <>
+      <h1>Logged In Screen</h1>
+      <button onClick={onClink}>Click to logout</button>
+    </>
+  );
+};
--- src/router/logged-out-router.tsx
+++ src/router/logged-out-router.tsx
@@ -1,3 +1,54 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
-export const LoggedOutRouter = () => <>"Logged Out"</>;
+type FormData = {
+  email: string;
+  password: string;
+};
+
+export const LoggedOutRouter = () => {
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  const onSubmit = () => {
+    console.log(watch(["email"]));
+  };
+
+  const onInvalid = () => {
+    console.log(errors);
+    console.log("Can't create an account.");
+  };
+
+  return (
+    <>
+      <h1>Logged Out Screen</h1>
+      <form onSubmit={handleSubmit(onSubmit, onInvalid)}>
+        <div>
+          <input
+            type="email"
+            required
+            placeholder="Email"
+            {...register("email", {
+              required: "Email is required.",
+              pattern: /^[A-Za-z0-9._%+-]+@gmail.com$/,
+            })}
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            placeholder="Password"
+            {...register("password", {
+              required: "Password is required.",
+            })}
+          />
+        </div>
+        <button className="bg-yellow-600 text-white">Submit</button>
+      </form>
+    </>
+  );
+};
