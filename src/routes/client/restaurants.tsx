3b0a028a8d32c57325680a0741c266694a63226b
beliemun <beliemun@paran.com>
2021-11-25 18:59:26 +0900

    Update restaurants.tsx

--- src/routes/client/restaurants.tsx
+++ src/routes/client/restaurants.tsx
@@ -4,7 +4,6 @@ import {
   seeRetaurants,
   seeRetaurantsVariables,
 } from "../../__generated__/seeRetaurants";
-import { couldStartTrivia } from "typescript";
 
 const SEE_RESTAURANT_QUERY = gql`
   query seeRetaurants($input: SeeRestaurantsInput!) {
@@ -54,17 +53,37 @@ export const Restaurants: React.FC = () => {
         />
       </form>
       {!loading && (
-        <div className="flex p-4 mx-auto ">
-          {data?.seeCategories.categories?.map((category, index) => (
-            <div key={index} className="flex-center flex-col mr-4">
+        <>
+          <div className="flex p-6 mx-auto ">
+            {data?.seeCategories.categories?.map((category, index) => (
               <div
-                className="w-16 h-16 bg-cover rounded-full border-4 border-gray-200 hover:border-green-500 cursor-pointer"
-                style={{ backgroundImage: `url(${category.coverImage})` }}
-              />
-              <span className="text-sm mt-2">{category.name}</span>
-            </div>
-          ))}
-        </div>
+                key={index}
+                className="group flex-center flex-col cursor-pointer mr-4"
+              >
+                <div
+                  className="w-16 h-16 bg-cover rounded-full border-4 border-gray-200 group-hover:border-green-500"
+                  style={{ backgroundImage: `url(${category.coverImage})` }}
+                />
+                <span className="text-sm mt-2">{category.name}</span>
+              </div>
+            ))}
+          </div>
+
+          <h2 className="text-2xl font-bold p-3">Recommended Retaurants</h2>
+          <div className="grid grid-cols-3 gap-3">
+            {data?.seeRestaurants.results?.map((restaurant, index) => (
+              <div key={index} className="w-full mb-3">
+                {console.log(restaurant)}
+                <div
+                  className="py-20 bg-cover bg-center"
+                  style={{ backgroundImage: `url(${restaurant.coverImage})` }}
+                ></div>
+                <h3>{restaurant.name}</h3>
+                <h4>{restaurant.category?.name}</h4>
+              </div>
+            ))}
+          </div>
+        </>
       )}
     </div>
   );
