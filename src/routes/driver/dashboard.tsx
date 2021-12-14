4b97ee9d01049bda7bda1794076149a29aa51568
beliemun <beliemun@paran.com>
2021-12-15 07:46:21 +0900

    #23.7 Driver Dashboard part Three

--- src/routes/driver/dashboard.tsx
+++ src/routes/driver/dashboard.tsx
@@ -1,25 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import GoogleMapReact from "google-map-react";
-// import { Helmet } from "react-helmet-async";
+import { Helmet } from "react-helmet-async";
+
+interface ICoords {
+  latitude: number;
+  longitude: number;
+}
 
 export const Dashboard: React.FC = () => {
+  const [driverCoords, setDriverCoords] = useState<ICoords>({
+    latitude: 0,
+    longitude: 0,
+  });
+  const [map, setMap] = useState<any>();
+  const [maps, setMaps] = useState<any>();
+  const onSuccess = (position: GeolocationPosition) => {
+    const {
+      coords: { latitude, longitude },
+    } = position;
+    setDriverCoords({ latitude, longitude });
+  };
+  const onError = (error: GeolocationPositionError) => {
+    console.log(error);
+  };
+  const onApiLoaded = ({ map, maps }: { map: any; maps: any }) => {
+    map.panTo(new maps.LatLng(driverCoords.latitude, driverCoords.longitude));
+    setMap(map);
+    setMaps(maps);
+  };
+  useEffect(() => {
+    navigator.geolocation.watchPosition(onSuccess, onError, {
+      enableHighAccuracy: true,
+    });
+  }, []);
+  useEffect(() => {
+    if (map && maps) {
+      map.panTo(new maps.LatLng(driverCoords.latitude, driverCoords.longitude));
+    }
+  }, [driverCoords.latitude, driverCoords.longitude]);
   return (
     <div
       className="overflow-hidden"
-      style={{ width: window.innerWidth, height: "90vh" }}
+      style={{ width: window.innerWidth, height: "50vh" }}
     >
-      {/* <Helmet>
+      <Helmet>
         <title>Dashboard | Uber Eats</title>
-      </Helmet> */}
+      </Helmet>
       <GoogleMapReact
-        defaultZoom={10}
+        defaultZoom={16}
         defaultCenter={{
-          lat: 59.95,
-          lng: 30.33,
+          lat: 37,
+          lng: 126,
         }}
-        bootstrapURLKeys={{ key: "AIzaSyDux6IrqN9iRWoBX-Nb5_LqN2vcvlo0t7k" }}
+        bootstrapURLKeys={{ key: "AIzaSyCSZWElP7VBeVAfaWKab6de0RaTAEoWMv4" }}
+        yesIWantToUseGoogleMapApiInternals
+        onGoogleApiLoaded={onApiLoaded}
       >
-        <h1>hello</h1>
+        <div
+          //@ts-ignore
+          lat={driverCoords.latitude}
+          lng={driverCoords.longitude}
+          className="flex justify-center items-center"
+        >
+          <span className="text-3xl pr-1 pb-1">🚗</span>
+        </div>
       </GoogleMapReact>
     </div>
   );
